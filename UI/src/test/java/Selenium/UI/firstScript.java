f4e9701649a71cf6776b50a5f8486aab696b9b90
Sri <Sri@LAPTOP-BTRBNINV>
2021-02-21 21:47:49 +0100

    All Files

---
+++ UI/src/test/java/Selenium/UI/firstScript.java
@@ -0,0 +1,21 @@
+package Selenium.UI;
+
+import static org.testng.Assert.assertEquals;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+public class firstScript {
+	
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Sri\\Desktop\\Self Learn\\Selenium Java\\Drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://automationpractice.com/index.php");
+		driver.manage().window().maximize();
+		String title = driver.getTitle();
+		Assert.assertEquals(title, "My Store");
+		driver.close();
+	}
+
+}
